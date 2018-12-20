f044473108c143f190233f9ee8837a83fd35e959
Bilal Turan <bilal_turan@hotmail.com>
2018-12-20 10:45:24 +0100

    .

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+//import {  ViewChild } from '@angular/core';
+//import { InfiniteScroll } from '@ionic/angular';
+
 import { WordTypes } from '../word-types.enum';
+import { Router } from '@angular/router';
+import { TodosService } from '../todos.service';
 
 @Component({
   selector: 'app-game',
@@ -9,11 +14,37 @@ import { WordTypes } from '../word-types.enum';
 export class GamePage implements OnInit {
 
   wordTypes = WordTypes;
+  todos: any[];
+  date: string;
 
-  constructor() { }
+  //@ViewChild(InfiniteScroll) infiniteScroll: InfiniteScroll;
+
+  constructor(private router: Router, private todoService: TodosService) {}
+
+  goToHome() {
+    this.router.navigate(["/home"]);
+  }
 
   ngOnInit() {
-
+    this.date = new Date().toISOString();
+    this.todoService.getTodos().subscribe(response => this.todos = <any[]>response);
   }
+
+  // loadData(event) {
+  //   setTimeout(() => {
+  //     console.log('Done');
+  //     event.target.complete();
+
+  //     // App logic to determine if all data is loaded
+  //     // and disable the infinite scroll
+  //     if (this.todos.length === 1000) {
+  //       event.target.disabled = true;
+  //     }
+  //   }, 500);
+  // }
+
+  // toggleInfiniteScroll() {
+  //   //infiniteScroll.disabled = !infiniteScroll.disabled;
+  // }
 
 }
