5b87a26c93aec0bbcdc45b52882a15b804f4f798
Bilal Turan <bilal_turan@hotmail.com>
2018-12-20 21:16:00 +0100

    ...

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 //import {  ViewChild } from '@angular/core';
 //import { InfiniteScroll } from '@ionic/angular';
 
-import { WordTypes } from '../word-types.enum';
-import { Router } from '@angular/router';
-import { TodosService } from '../todos.service';
+import { WordTypes } from "../word-types.enum";
+import { Router } from "@angular/router";
+import { TodosService } from "../todos.service";
+import { NewsapiService } from "../newsapi.service";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.page.html',
-  styleUrls: ['./game.page.scss'],
+  selector: "app-game",
+  templateUrl: "./game.page.html",
+  styleUrls: ["./game.page.scss"]
 })
 export class GamePage implements OnInit {
-
   wordTypes = WordTypes;
   todos: any[];
   date: string;
+  news: any[];
+  news$: Observable<any>;
+
+  page = 1;
+  totalItems = 0;
 
   //@ViewChild(InfiniteScroll) infiniteScroll: InfiniteScroll;
 
-  constructor(private router: Router, private todoService: TodosService) {}
+  constructor(
+    private router: Router,
+    private todoService: TodosService,
+    private newsService: NewsapiService
+  ) {}
 
   goToHome() {
     this.router.navigate(["/home"]);
@@ -27,24 +38,31 @@ export class GamePage implements OnInit {
 
   ngOnInit() {
     this.date = new Date().toISOString();
-    this.todoService.getTodos().subscribe(response => this.todos = <any[]>response);
+    this.newsService
+      .getNews(this.page)
+      .subscribe(response => {
+        this.totalItems = response.totalResults;
+        this.news = <any[]>(response.articles);
+      });
   }
 
-  // loadData(event) {
-  //   setTimeout(() => {
-  //     console.log('Done');
-  //     event.target.complete();
+  loadMoreNews(event) {
+    console.log(event);
 
-  //     // App logic to determine if all data is loaded
-  //     // and disable the infinite scroll
-  //     if (this.todos.length === 1000) {
-  //       event.target.disabled = true;
-  //     }
-  //   }, 500);
-  // }
+    this.page++;
+    this.newsService.getNews(this.page).pipe(
+      map(resp => (<any>resp).articles)
+    ).subscribe(response => {
+      event.target.complete();
+      this.news = this.news.concat(<any[]>response);
+
+      if (this.news && (this.news.length === this.totalItems)) {
+           event.target.disabled = true;
+      }
+    });
+  }
 
   // toggleInfiniteScroll() {
   //   //infiniteScroll.disabled = !infiniteScroll.disabled;
   // }
-
 }
